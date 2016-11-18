d6e609708efe2af45d58701c9c92a7d25a4935bd
pattywgm <pattywgm@163.com>
2016-11-18 16:43:56 +0800

    增加Cassandra cluster集群节点配置

--- src/main/scala/com/pattywgm/quill/modules/AppDataBase.scala
+++ src/main/scala/com/pattywgm/quill/modules/AppDataBase.scala
@@ -1,7 +1,9 @@
 package com.pattywgm.quill.modules
 
-import com.pattywgm.quill.models.{ConcreteUserStat, ConcreteCqlNews, ConcreteSqlNews}
-import io.getquill.{CamelCase, CassandraAsyncContext, FinagleMysqlContext}
+import java.io.File
+import com.pattywgm.quill.models.{ConcreteCqlNews, ConcreteSqlNews, ConcreteUserStat}
+import com.typesafe.config.ConfigFactory
+import io.getquill.{CassandraContextConfig, CamelCase, CassandraAsyncContext, FinagleMysqlContext}
 
 /**
   * Version: 1.1
@@ -10,12 +12,28 @@ import io.getquill.{CamelCase, CassandraAsyncContext, FinagleMysqlContext}
   * Desc:
   */
 class AppDataBase {
-  lazy val mariadb: FinagleMysqlContext[CamelCase] = new FinagleMysqlContext[CamelCase]("mariadb")
-  lazy val ctx: CassandraAsyncContext[CamelCase] = new CassandraAsyncContext[CamelCase]("dsc")
+
+  // load configurations from local file
+  val parsedConfig = ConfigFactory.parseFile(new File("/Users/pattywgm/SelfProject/LearningAndPractice/src/main/resources/application.properties"))
+  val conf = ConfigFactory.load(parsedConfig)
+  val ctxConfig = new CassandraContextConfig(conf.getConfig("csx"))
+  ctxConfig.builder.addContactPoint("127.0.0.2")
+  val ctx: CassandraAsyncContext[CamelCase] = new CassandraAsyncContext[CamelCase](ctxConfig)
+
+  lazy val mariadb: FinagleMysqlContext[CamelCase] = new FinagleMysqlContext[CamelCase](conf.getConfig("mdb"))
+
+  /**
+    * // load configurations from class path
+    * lazy val mariadb: FinagleMysqlContext[CamelCase] = new FinagleMysqlContext[CamelCase]("mdb")
+    * lazy val ctx: CassandraAsyncContext[CamelCase] = new CassandraAsyncContext[CamelCase]("csx")
+    */
+//  val cluster = Cluster.builder().addContactPoints("127.0.0.1", "127.0.0.2").build()
+
 
   object newsSql extends ConcreteSqlNews(mariadb)
 
   object newsCql extends ConcreteCqlNews(ctx)
 
   object userCql extends ConcreteUserStat(ctx)
+
 }
